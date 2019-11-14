b5fc55361347ca4b03b758ffae558ee065943d40
Alex <alexander.mattsson@hotmail.com>
2019-11-14 10:39:45 +0100

    Uppdated routes

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+import 'home.dart';
 
+void main() => runApp(MaterialApp(
+    initialRoute: "/",
+    routes: {
+      "/": (context) => Home(),
+    },
 ));
