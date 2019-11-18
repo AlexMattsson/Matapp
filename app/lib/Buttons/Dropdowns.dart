fa444c38aa0fdba76b1e10fbab726a7bba781000
Alex <alexander.mattsson@hotmail.com>
2019-11-18 09:42:00 +0100

    Uppdated indent

--- app/lib/Buttons/Dropdowns.dart
+++ app/lib/Buttons/Dropdowns.dart
@@ -1,45 +1,61 @@
 import 'package:flutter/material.dart';
 
 class ChooseClassDropdown extends StatefulWidget {
-  @override
-  _ChooseClassDropdownState createState() {
-    return _ChooseClassDropdownState();
-  }
+
+    @override
+    _ChooseClassDropdownState createState() {
+        return _ChooseClassDropdownState();
+    }
+
 }
 
-class _ChooseClassDropdownState extends State<ChooseClassDropdown> {
-  String _value;
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: DropdownButton<String>(
-        icon: Icon(Icons.arrow_downward),
-        style: TextStyle(
-          color: Colors.white,
-        ),
-        items: [
-          DropdownMenuItem<String>(
-            child: Text('Item 1'),
-            value: 'one',
-          ),
-          DropdownMenuItem<String>(
-            child: Text('Item 2'),
-            value: 'two',
-          ),
-          DropdownMenuItem<String>(
-            child: Text('Item 3'),
-            value: 'three',
-          ),
-        ],
-        onChanged: (String value) {
-          setState(() {
-            _value = value;
-          });
-        },
-        hint: Text('Select Item'),
-        value: _value,
-      ),
-    );
-  }
+class _ChooseClassDropdownState extends State<ChooseClassDropdown> {
+    String _value;
+
+    List<String> classes = ["TE15", "TE16", "TE17", "TE18", "TE19"];
+
+    List<DropdownMenuItem<String>> getMenuItems() {
+        List<DropdownMenuItem<String>> items = [];
+        DropdownMenuItem<String> item;
+
+        for (int i=0;i<classes.length;i++) {
+            int value = i+1;
+            item = DropdownMenuItem<String>(
+                child: Text(classes[i]),
+                value: "$value",
+            );
+            items.add(item);
+        }
+        return items;
+
+    }
+
+
+
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: DropdownButton<String>(
+                icon: Icon(Icons.arrow_downward),
+                style: TextStyle(
+                    color: Colors.grey[800],
+                ),
+
+                items: getMenuItems(),
+                onChanged: (String value) {
+                    setState(() {
+                        _value = value;
+                    });
+                },
+                hint: Text(
+                    "Choose class",
+                    style: TextStyle(
+                        color: Colors.grey[800],
+                    ),
+                ),
+                value: _value,
+            ),
+        );
+    }
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,19 +4,10 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() => runApp(MaterialApp(
-<<<<<<< HEAD
-    initialRoute: "/splash",
+    initialRoute: "/",
     routes: {
       "/": (context) => Home(),
       "/splash": (context) => SplashScreen(),
 
     },
 ));
-=======
-      initialRoute: "/",
-      routes: {
-        "/": (context) => Home(),
-        "/splash": (context) => SplashScreen(),
-      },
-    ));
->>>>>>> ca27387fa94e675f8a61a48a7996b4568b6d49cc
