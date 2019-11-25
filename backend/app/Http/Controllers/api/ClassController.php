cc7c7320ad3123706a405bfca11b628995753ea6
Abborren <elias.renman00@gmail.com>
2019-11-25 15:31:08 +0100

    added models and stuff

---
+++ backend/app/Http/Controllers/api/ClassController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+}
