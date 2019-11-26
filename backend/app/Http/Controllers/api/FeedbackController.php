e96457bcdbaf96248c23b20054a5223afe6483a3
Abborren <elias.renman00@gmail.com>
2019-11-26 12:58:31 +0100

    added validation

--- backend/app/Http/Controllers/api/FeedbackController.php
+++ backend/app/Http/Controllers/api/FeedbackController.php
@@ -16,6 +16,22 @@ class FeedbackController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
+        $validated = $this->validateRequest($request);
+        
+    }
+
+    private function validateRequest(Request $request)
+    {
+        return $this->validate($request, [
+            'class' => 'required|integer|exists:schoolclasses,id',
+            'diet' => 'required|string|max:24',
+            'user' => 'required|string|max:36',
+            
+            'staff_informed' => 'nullable|boolean',
+            'rating' => 'required|integer|max:4',
+            'cause' => 'nullable|string|max:20',
+            'additional_feedback' => 'nullable|string|max:128',
+        ]);
     }
 }
--- backend/database/migrations/2019_11_25_121247_create_feedback_table.php
+++ backend/database/migrations/2019_11_25_121247_create_feedback_table.php
@@ -16,11 +16,12 @@ class CreateFeedbackTable extends Migration
         Schema::create('feedback', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('class_id');
+            $table->string('user_id', 36);
             $table->string('diet', 24);
             $table->string('resturant', 24);
-            $table->string('user_id', 36);
-            $table->tinyInteger('rating', false, true);
+            
             $table->boolean('staff_informed')->default(0);
+            $table->tinyInteger('rating', false, true);
             $table->string('cause', 20)->nullable();
             $table->string('additional_feedback', 128)->nullable();
             $table->timestamps();
