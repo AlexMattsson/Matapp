e8fe0901a26300a9cd3b54c62d7806b08806f4c2
Abborren <elias.renman00@gmail.com>
2019-12-02 10:15:10 +0100

    done did shit

--- app/lib/Widgets/reasonFieldWidget.dart
+++ app/lib/Widgets/reasonFieldWidget.dart
@@ -3,38 +3,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ReasonFieldWidget extends StatefulWidget {
-    @override
-    ReasonFieldState createState() => new ReasonFieldState();
+  @override
+  ReasonFieldState createState() => new ReasonFieldState();
 
 }
 
 class ReasonFieldState extends State<ReasonFieldWidget> {
-    String fieldReasonValue;
-    TextEditingController _controller = TextEditingController();
+  String _fieldReasonValue;
+  TextEditingController _controller = TextEditingController();
 
+  @override
+  initState() {
+    super.initState();
+    PersistentStorage.get('reasonField').then((value) {
+      setState(() {
+        _fieldReasonValue = value;
+        _controller.text = value;
+      });
+    });
 
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            width: 200,
-            child: TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(
-                    labelText: "Ytteligare feedback:",
-                    labelStyle: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                        letterSpacing: 1.0,
-                    ),
-                ),
-                onChanged: (String newValue) {
-                    setState(() {
-                        fieldReasonValue = newValue;
-                        // Call the storage update
-                        PersistentStorage.set('reasonField', fieldReasonValue);
-                    });
-                },
-            ),
-        );
-    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    //print('Field value: $_fieldValue');
+    return Container(
+      width: 200,
+      child: TextFormField(
+        controller: _controller,
+        decoration: const InputDecoration(
+          labelText: 'Ytterligare anledningar:',
+        ),
+        onChanged: (String newValue) {
+          setState(() {
+            _fieldReasonValue = newValue;
+            // Call the storage update
+            PersistentStorage.set('reasonField', _fieldReasonValue);
+          });
+        },
+      ),
+    );
+  }
 }
