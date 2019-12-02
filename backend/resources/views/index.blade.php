06f7620910ab58d2129691714bfccdd124d76a57
august.moller <ag.man@live.se>
2019-12-02 12:38:41 +0100

    Co-authored-by: Elias Renman <elias.renman00@gmail.com>

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -28,57 +28,15 @@
     <a class="btn btn-primary" href="/options.blade.php" role="button">
         options
     </a>
-
+    <script>
+        window.data = JSON.parse(`{!! $data ?? ''!!}`);
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script type="text/javascript">
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'doughnut',
-            // The data for our dataset
-            data: {
-                labels: ['Bra', 'Ok', 'Sådär', 'Dåligt'],
-                datasets: [{
-                    label: 'rating',
-                    backgroundColor: ["#388E3C", "#AFB42B", "#FF7043", "#C62828"],
-                    borderColor: ["#388E3C", "#AFB42B", "#FF7043", "#C62828"],
-                    data: [1, 1, 1, 1],
-                }]
-            },
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.min.js""></script>
 
-            // Configuration options go here
-            options: {}
-        });
-
-        var ctx = document.getElementById('myChart2').getContext('2d');
-        var chart2 = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'doughnut',
-            // The data for our dataset
-            data: {
-                labels: ['Bra', 'Ok', 'Sådär', 'Dåligt'],
-                datasets: [{
-                    label: 'rating',
-                    backgroundColor: ["#388E3C", "#AFB42B", "#FF7043", "#C62828"],
-                    borderColor: ["#388E3C", "#AFB42B", "#FF7043", "#C62828"],
-                    data: [4, 2, 2, 4],
-                }]
-            },
-
-            // Configuration options go here
-            options: {}
-        });
-    </script>
-    </canvas>
-    </script>
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+    <script src="/js/index.js"> </script> <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
