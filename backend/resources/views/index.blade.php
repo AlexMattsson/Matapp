ecb87e8661e0852908d512d79006764d5e1b1d70
Abborren <elias.renman00@gmail.com>
2019-12-03 11:12:36 +0100

    added vue
    Co-authored-by: Agman10 <ag.man@live.se>

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,28 +1,23 @@
 @extends('layout.base')
 @section('title', 'Home')
-<body>
-    @section('content')
-    <div class="col-md-12">
+@section('content')
+    <div class="col-md-12" id="app">
         <div class="row">
             <div class="col-md-6">
                 <p class="text-center" id="olearys">Olearys</p>
-                <canvas id="myChart" width="20" height="10"></canvas>
+                <chart :data='@json($data)' :resturant="'Olearys'" ></chart>
             </div>
             <div class="col-md-6">
                 <p class="text-center" id="greek">Greek grill</p>
-                <canvas id="myChart2" width="20" height="10"></canvas>
+                <chart :data='@json($data)' :resturant='"Greek Grill"'></chart>
             </div>
         </div>
     </div>
-      
-    @endsection
-    @section('scripts')
-        @parent
-        <script>
-            window.data = JSON.parse(`{!! $data ?? ''!!}`);
-        </script>
-        <script src="/js/index.js"></script>
-    @endsection
-</body>
-
-</html>
+    
+@endsection
+@section('scripts')
+    <script>
+        window.data = JSON.parse(`{!! $data ?? ''!!}`);
+    </script>
+    <script src="/js/index.js"></script>
+@endsection
--- backend/resources/views/layout/base.blade.php
+++ backend/resources/views/layout/base.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <script src="chart.js"></script>
     <title>@yield('title')</title>
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -18,11 +17,11 @@
   
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">@yield('title', 'placeholder') <span class="sr-only">(current)</span></a>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/classes">Klasser</a>
+          <a class="nav-link" href="/class">Klasser</a>
         </li>
         
     </div>
@@ -30,11 +29,13 @@
 <body>
     
     @yield('content')
-        
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+        <script src="/js/manifest.js"></script>
+        <script src="/js/vendor.js"></script>
+        <script src="/js/app.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.min.js""></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> --}}
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     @yield('scripts')
 </body>
