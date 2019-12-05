bfd6b34f2c135bdc3441d4d72b5f7a96c8cdb219
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-12-05 12:49:39 +0100

    Update ExternalKeyMiddleware.php

--- backend/app/Http/Middleware/ExternalKeyMiddleware.php
+++ backend/app/Http/Middleware/ExternalKeyMiddleware.php
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-
-class ExternalKeyMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        $validated = Validator::make($request->all(),[
-            'access_key' => 'bail|required|exists:accesskeys'
-        ]);
-        return count($validated->errors()) > 0 ? 
-            $validated->errors() : $next($request);
-    }
-}
-=======
 <?php
 
 namespace App\Http\Middleware;
@@ -52,4 +24,3 @@ class ExternalKeyMiddleware
             abort(403, $validated->errors()) : $next($request);
     }
 }
->>>>>>> 1c4ced3d95c0f7fbb12a988cce89ff4e3eba83d8
