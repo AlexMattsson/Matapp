14661898698146d7c9a4bb38213c9a89a4574369
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-12-05 12:44:27 +0100

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-
-$router->get('/classes/get', 'api\ClassController@index');
-
-$router->post('/feedback/store', 'api\FeedbackController@store');
-=======
 <?php
 
 
@@ -14,4 +6,3 @@ $router->get('/classes/get', 'api\ClassController');
 $router->post('/feedback/store',
     ['middleware' => 'access_key', "uses" =>
     'api\FeedbackController']);
->>>>>>> 1c4ced3d95c0f7fbb12a988cce89ff4e3eba83d8
