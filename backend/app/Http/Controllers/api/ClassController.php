ec850757839a0d397f68df6276b40273d116e714
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-12-05 12:46:39 +0100

    Update ClassController.php

--- backend/app/Http/Controllers/api/ClassController.php
+++ backend/app/Http/Controllers/api/ClassController.php
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-<?php
 
-namespace App\Http\Controllers\api;
-
-use App\Http\Controllers\Controller;
-use App\SchoolClasses;
-use Illuminate\Http\Request;
-
-class ClassController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return SchoolClasses::all();
-    }
-}
-=======
 <?php
 
 namespace App\Http\Controllers\api;
@@ -40,4 +19,3 @@ class ClassController extends Controller
         return SchoolClasses::all();
     }
 }
->>>>>>> 1c4ced3d95c0f7fbb12a988cce89ff4e3eba83d8
