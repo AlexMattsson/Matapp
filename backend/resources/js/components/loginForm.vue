23c96fafc7750ac983e922e02b6203514720200d
leosidebo <abletoirper@gmail.com>
2020-01-23 10:23:35 +0100

    Vue for login page. Post request included as well as redirect

---
+++ backend/resources/js/components/loginForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="text" v-model="email" name="email" />
+        <input type="password" v-model="password" name="password" />
+        <button @click="login">Skicka in</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '', 
+            password: ''
+        }
+    },
+
+    methods: {
+        async login() {
+            let { data } = await axios.post('/login/auth', {
+                    "email": this.email,
+                    "password": this.password
+                });
+            window.localStorage.setItem('token', data);
+            window.location.replace('/');
+        },
+    },
+}
+</script>
