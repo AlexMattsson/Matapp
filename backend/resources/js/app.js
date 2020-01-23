425bb29b190467d4400558cf74bfaaca0b286114
leosidebo <abletoirper@gmail.com>
2020-01-23 10:24:10 +0100

    Added new vue components to app js

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -6,28 +6,29 @@ window.Vue = require('vue');
 
 window.axios.defaults.headers.common = {
     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-    'X-Requested-With': 'XMLHttpRequest'
+    'X-Requested-With': 'XMLHttpRequest',
 };
 import infotree from './components/informationTree';
 import classPage from './components/classPage';
+import loginForm from './components/loginForm';
 import VModal from 'vue-js-modal';
 Vue.component('info-tree', infotree);
 Vue.component('class-page', classPage);
+Vue.component('login-form', loginForm);
 import VCalendar from 'v-calendar';
- 
 
 Vue.use(VCalendar, {
-  formats: {
-    title: 'MMMM YYYY',
-    weekdays: 'W',
-    navMonths: 'MMM',
-    input: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
-    dayPopover: 'L',
-  }
+    formats: {
+        title: 'MMMM YYYY',
+        weekdays: 'W',
+        navMonths: 'MMM',
+        input: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
+        dayPopover: 'L',
+    },
 });
 
 Vue.use(VModal, { dynamic: true, dynamicDefaults: { clickToClose: false } });
 
 new Vue({
     el: '#app',
-})
+});
