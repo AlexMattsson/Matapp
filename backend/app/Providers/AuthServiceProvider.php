9ce898fd8ac26d8b40e781e709ed8fdfcdde255a
leosidebo <abletoirper@gmail.com>
2020-01-30 10:09:29 +0100

    Removed unused code

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -30,14 +30,6 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        /*Gate::define('get-attempt', $_GET, function ($user) {
-            if ($user->getJWTIdentifier() === null) {
-                return null;
-            } else {
-                return $user;
-            }
-        }); */
-
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->input('api_token')) {
                 return User::where('api_token', $request->input('api_token'))->first();
